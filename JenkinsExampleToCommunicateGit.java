7745e9f32339cb8d89f1ae610f305e4ec9927010
spmselva <64827322+spmselva@users.noreply.github.com>
2020-05-28 18:04:00 +0530

    Initial Commit

---
+++ JenkinsExampleToCommunicateGit.java
@@ -0,0 +1,9 @@
+public class JenkinsExampleToCommunicateGit
+{
+public static void main(String [] args)
+{
+System.out.println("Hello Selva");
+System.out.println("Hello Raja");
+System.out.println("Hello ALL");
+}
+}
