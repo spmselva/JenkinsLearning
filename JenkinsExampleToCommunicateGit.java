f1c84c1c1c6fe9b6dbd77ab0091d8c5f8dbe2321
spmselva <64827322+spmselva@users.noreply.github.com>
2020-06-03 00:29:52 +0530

    added one line

--- JenkinsExampleToCommunicateGit.java
+++ JenkinsExampleToCommunicateGit.java
@@ -5,5 +5,6 @@ public static void main(String [] args)
 System.out.println("Hello Selva");
 System.out.println("Hello Raja");
 System.out.println("Hello ALL");
+System.out.println("Edited for Testing
 }
 }
