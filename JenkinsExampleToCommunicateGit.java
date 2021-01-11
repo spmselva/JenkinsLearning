caaa067712df9a1aefd3155e3b56296adbc9b7af
spmselva <64827322+spmselva@users.noreply.github.com>
2021-01-11 20:07:55 +0530

    Update JenkinsExampleToCommunicateGit.java
    Added today by selva

--- JenkinsExampleToCommunicateGit.java
+++ JenkinsExampleToCommunicateGit.java
@@ -7,5 +7,6 @@ System.out.println("Hello Raja");
 System.out.println("Hello ALL");
 System.out.println("Edited for Testing");
 System.out.println("Added on eleventh january");
+System.out.println("Working fine push happen in github");
 }
 }
